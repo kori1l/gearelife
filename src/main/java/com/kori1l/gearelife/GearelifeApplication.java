421cf6abc4889ec8d12612f4b988acda2da6fa6e
kori1l <kori1l@yandex.ru>
2024-03-14 22:30:46 +0300

    init

---
+++ src/main/java/com/kori1l/gearelife/GearelifeApplication.java
@@ -0,0 +1,13 @@
+package com.kori1l.gearelife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearelifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearelifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kori1l/gearelife/GearelifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kori1l.gearelife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearelifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
